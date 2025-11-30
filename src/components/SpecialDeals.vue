864911a8026a707be61a6a4a11e9b5ed68616553
Raymond <raymondtdev@gmail.com>
2025-11-30 15:11:26 +0200

    fix

--- src/components/SpecialDeals.vue
+++ src/components/SpecialDeals.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted, computed } from 'vue';
   import menuItems from '../menu-items.json';
 
   const thumbnailUrls = computed(() => {
